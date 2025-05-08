81a199805b46720306147dad30a01bcab0ad4669
Sotokc2025 <alexa_soy@hotmail.com>
2025-05-07 20:54:47 -0600

    subido

--- semana1/clase2_tarea.js
+++ semana1/clase2_tarea.js
@@ -21,4 +21,23 @@ async function main() {
 
 }
 
+
+main();
+
+const { ask } = require('../helpers/input.js');
+
+async function main() {
+    const temperatura = Number(await ask('¿Cuál es la temperatura actual?'));
+
+    if (temperatura < 10 ) {
+        console.log('Hace frío.');
+    } else if (temperatura >= 10 && temperatura < 20) {
+        console.log('Esta templado.');
+    } else if (temperatura >= 20 && temperatura < 30) {
+        console.log('Hace calor.');
+    } else if (temperatura > 30) {
+        console.log('Hace mucho calor.');
+    }
+
+}
 main();
--- semana1/ejercicio1.js
+++ semana1/ejercicio1.js
@@ -1,14 +1,14 @@
 // Clase 1: ¿Qué es programar? + Variables y Tipos de Datos
 
+
 const { ask } = require('../helpers/input');
-
 async function main() {
-  const name = await ask("¿Cómo te llamas? ");
-  const age = Number(await ask("¿Cuántos años tienes? "));
-  // TODO : Agregar pregunta para comida favorita
+  const name = await ask('¿Cuál es tu nombre?');
+  const age = Number(await ask('¿Cuál es tu edad?'));
+  const favoriteFood = await ask('¿Cuál es tu comida favorita?'); 
 
-  console.log("Hola " + name + ", tienes " + age + " años.");
-  //console.log(`Tu comida favorita es ${comida}.`);
+  console.log(`Hola Karla ${name}, tienes ${age}30 años.`);
+  console.log(`Tu comida favorita es ${favoriteFood}la pasta.`);
 }
 
 main();
