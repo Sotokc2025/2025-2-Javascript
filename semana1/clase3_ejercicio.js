ea1365d7488d7219e264974a5dc4d5b5001b9ba1
Sotokc2025 <alexa_soy@hotmail.com>
2025-05-08 20:05:42 -0600

    Ejercicio3 resuelto

--- semana1/clase3_ejercicio.js
+++ semana1/clase3_ejercicio.js
@@ -1,22 +1,60 @@
 // Día de la semana según número
 // Función que devuelve el día de la semana según un número del 1 al 7
 // Ejercicio: Crear un programa que reciba un número del 1 al 7 y devuelva el día de la semana correspondiente (1 = Lunes, 2 = Martes, etc.).
+// TODO : Implementar la lógica para mo/strar el día de la semana según el número ingresado
+  // Ejemplo: Si el usuario ingresa 1, debe mostrar "Lunes"
 
-const { ask } = require('../helpers/input');
+const {ask} = require('../helpers/input');
 
 async function main() {
   const dia = Number(await ask("Ingresa un número del 1 al 7:"));
   const diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
 
-  // TODO : Implementar la lógica para mostrar el día de la semana según el número ingresado
-  // Ejemplo: Si el usuario ingresa 1, debe mostrar "Lunes"
-
+  
+  
   if (dia >= 1 && dia <= 7) {
     console.log(diasSemana[dia - 1]);
   }
   else {
-    console.log("Numero no válido");
-  }
+    console.log("Numero no válido"); //
 }
+ 
 
-main();
+ main();
+
+
+
+switch (dia ){
+ case 1:
+ console.log(diasSemana[0]);
+ break;
+ case 2:
+ console.log(diasSemana[1]);
+ break;
+ case 3:
+ console.log(diasSemana[2]);
+ break;
+ case 4:
+ console.log(diasSemana[3]);
+ break;
+ case 5:
+ console.log(diasSemana[4]);
+ break;
+ case 6:
+ console.log(diasSemana[5]);
+ break;
+ case 7:
+ console.log(diasSemana[6]);
+ break;
+ default:
+ console.log("Número no válido");
+ break;
+ }
+}
+ 
+
+ main();
+ 
+
+
+
