a6eb05aa9cd9d2ac0fa8560582df6cdf3547a82a
Sotokc2025 <alexa_soy@hotmail.com>
2025-05-17 17:52:40 -0600

    Resuelvo tarea clase3_tarea.js

--- semana2/clase3_tarea.js
+++ semana2/clase3_tarea.js
@@ -9,3 +9,26 @@
 // Cuenta cuántos tienen 18 años o más.
 //
 // Muestra el total.
+
+const { ask } = require('../helpers/input');
+
+async function main() {
+    let edades = [];
+    let cantidades = Number(await ask('¿Cuántas edades van a ingresar? '));
+    for (let i = 0; i < cantidades; i++) {
+        let edad = Number(await ask(`Ingresa la edad # ${i+1}: `));
+        edades.push(edad);
+
+    }
+    let puedenVotar = 0;
+    for (let i = 0; i < edades.length; i++) {
+        if (edades[i] >= 18) {
+            puedenVotar++;
+        }
+    }
+
+    console.log(`De las ${edades.length} personas, ${puedenVotar} pueden votar (porque tienen 18 años o más).`);
+}
+
+main();
+
