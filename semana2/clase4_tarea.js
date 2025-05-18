464147ea8e3a91647172f1525ac3550ee6a91d68
Sotokc2025 <alexa_soy@hotmail.com>
2025-05-17 20:36:29 -0600

    Resuelta tarea clase4_tarea.js

--- semana2/clase4_tarea.js
+++ semana2/clase4_tarea.js
@@ -12,3 +12,52 @@
 //Promedio general
 //
 //La calificación más alta y la más baja
+
+const { ask } = require('../helpers/input');
+
+async function main() {
+
+    const calificaciones = [60, 80, 70, 50, 100, 90];
+
+    function analizarCalificaciones(calificaciones) {
+        let aprobados = 0;
+        let reprobados = 0;
+        let total = 0;
+        let mayor = calificaciones[0];
+        let menor = calificaciones[0];
+
+
+        for (let i = 0; i < calificaciones.length; i++) {
+            total = total + calificaciones[i];
+
+            if (calificaciones[i] >= 70) {
+                aprobados++
+            } else {
+                reprobados++;
+            }
+            if (calificaciones[i] > mayor)
+                mayor = calificaciones[i];
+            if (calificaciones[i] < menor)
+                menor = calificaciones[i];
+
+        }
+
+
+        const promedio = total / calificaciones.length;
+        return { aprobados, reprobados, promedio, mayor, menor };
+
+        
+    }
+    
+    const resultado = analizarCalificaciones(calificaciones);
+    console.log(`La cantidad de personas aprobadas son: ${resultado.aprobados}`);
+    console.log(`La catidad de reprobados son: ${resultado.reprobados}`);
+    console.log(`El promedio de calificaciones general de los alumnos es: ${resultado.promedio}`);
+    console.log(`La calificación más alta es: ${resultado.mayor}`);
+    console.log(`La calificación más baja es: ${resultado.menor}`);
+
+
+
+}
+main()
+
